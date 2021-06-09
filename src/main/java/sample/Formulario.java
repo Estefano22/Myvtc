9a7415cd20a10c2c202833b722cdb91d9bee11f9
Estefano22 <sandulescustefano8@gmail.com>
2021-06-09 13:30:56 +0200

    seguir haciendo

--- src/main/java/sample/Formulario.java
+++ src/main/java/sample/Formulario.java
@@ -1,4 +1,41 @@
 package sample;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+
+import java.util.ArrayList;
+
 public class Formulario {
+
+
+    @FXML
+    TextField TextFieldSalidaFechaHora, TextFieldDestinoFechaHora, TextFieldSalida,TextFieldDestino, TextFieldIngresos, TextFieldMercanciaTransportada, TextFieldToneladas,TextFieldKilometros, TextFieldLitros;
+
+    @FXML
+    Button BottonEnviar;
+
+    ArrayList<Label> listadeTextField = new ArrayList();
+
+    @FXML
+    public void initialize(){
+
+        listadeTextField.add(TextFieldSalidaFechaHora);
+        listadeTextField.add(TextFieldDestinoFechaHora);
+        listadeTextField.add(TextFieldSalida);
+        listadeTextField.add(TextFieldDestino);
+        listadeTextField.add(TextFieldIngresos);
+        listadeTextField.add(TextFieldMercanciaTransportada);
+        listadeTextField.add(TextFieldToneladas);
+        listadeTextField.add(TextFieldKilometros);
+        listadeTextField.add(TextFieldLitros);
+
+        Binder.binderBitacora(TextFieldSalidaFechaHora,TextFieldDestinoFechaHora,estrella1);
+
+
+
+    }
+
+
 }
